2292141af872e1d4db7b045755ef09c34cec32c8
NotNorom <NotNorom@users.noreply.github.com>
2026-03-28 16:01:32 +0000

    deploy: 6f29d65a0e7f838799e59ba40016780fed74bc4a

---
+++ current/trait.impl/futures_task/future_obj/trait.UnsafeFutureObj.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["futures",[]],["futures_task",[]],["futures_util",[]],["poise",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[14,20,20,13]}
